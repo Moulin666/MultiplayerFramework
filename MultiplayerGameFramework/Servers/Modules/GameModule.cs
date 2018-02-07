972a7e9700e403333b9db76914c897e7e7763b11
Roman <thirtybeltmusicgroup@gmail.com>
2018-02-07 23:13:54 +0300

    Part of last commit
    Signed-off-by: Roman <thirtybeltmusicgroup@gmail.com>

---
+++ MultiplayerGameFramework/Servers/Modules/GameModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+
+namespace Servers.Modules
+{
+	public class GameModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
+
+
+		}
+	}
+}
